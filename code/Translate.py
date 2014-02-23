175d160510290360c8b549cf1578307448dfb0cb
bridge-eimon <beimon@stanford.edu>
2014-02-23 15:12:02 -0800

    changes to dictionary, show part of speech

--- code/Translate.py
+++ code/Translate.py
@@ -7,17 +7,21 @@ def baseLineTranslations(fileName):
 	dictionary = Util.readDict()
 	for line in corpus_file:
 		translation = []
+		wordTypes = []
 		for word in line.split():
 			matches = re.findall("(\W*)(\w+)(\W*)",word)
 			if matches[0][0] == "\'":
 				trans = dictionary[matches[0][0]+matches[0][1]]
 				translation.append(trans[0]+matches[0][2])
+				wordTypes.append(trans[1])
 			else:
 				trans = dictionary[matches[0][1]]
 				translation.append(matches[0][0]+trans[0]+matches[0][2])
+				wordTypes.append(trans[1])
 		print " "
 		print line
 		print "Translation: "+" ".join(translation)
+		print "Word Types:"+" ".join(wordTypes)
 		print "---------------------------------------------------------"
 
 if __name__ == "__main__":
