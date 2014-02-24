88d9d23f8e0b7f7a20dd530c47c1eae5247526d6
David Khavari <dkhavari@gmail.com>
2014-02-23 18:55:27 -0800

    Added question context rule.

--- code/StrategiesOne.py
+++ code/StrategiesOne.py
@@ -2,7 +2,7 @@ import Util
 
 
 # Replace "full" with "full of."
-def applyFullContext(sentence):
+def applyFullContextRule(sentence):
 	new_sentence = []
 	# Minus one indexing for lookahead.
 	for i in xrange(len(sentence) - 1):
@@ -16,7 +16,7 @@ def applyFullContext(sentence):
 	return new_sentence
 
 # Replace "from is" with "from this is." Afrikaans doesn't always include "this."
-def applyFromContext(sentence):
+def applyFromContextRule(sentence):
 	new_sentence = []
 	# Minus one indexing for lookahead.
 	for i in xrange(len(sentence) - 1):
@@ -29,3 +29,18 @@ def applyFromContext(sentence):
 	new_sentence.append(sentence[-1])
 	return new_sentence
 
+def applyQuestionContextRule(sentence):
+	new_sentence = []
+	# Minus one indexing for lookahead.
+	for i in xrange(len(sentence) - 1):
+		new_sentence.append(sentence[i])
+		word = sentence[i][0]
+		next_word = sentence[i + 1][0]
+		# If we have "how you", change it to "how do you."
+		if sentence[i][1] == 'Q':
+			if next_word == 'you':
+				new_sentence.append(('do', 'V'))
+			else:
+				new_sentence.append(('does', 'V'))
+	new_sentence.append(sentence[-1])
+	return new_sentence
